f120b90f1ef5754ecaf2623875a98118856068d4
kes <2604338097@qq.com>
2025-11-06 16:49:36 +0800

    1

--- User/main.c
+++ User/main.c
@@ -9,57 +9,59 @@ void SysTick_Handler(void)
 
 void Systick_Init(void)
 {
-    // Configure SysTick for 1ms interrupt
-    // SystemCoreClock = 168000000
-    // SysTick_Config(SystemCoreClock / 1000) generates 1ms interrupt
-    
-    if (SysTick_Config(SystemCoreClock / 1000)) {
-        // Error handling, stay here
-        while (1);
-    }
+  // Configure SysTick for 1ms interrupt
+  // SystemCoreClock = 168000000
+  // SysTick_Config(SystemCoreClock / 1000) generates 1ms interrupt
 
-    // Set SysTick interrupt priority to highest
-    NVIC_SetPriority(SysTick_IRQn, 0);
+  if (SysTick_Config(SystemCoreClock / 1000))
+  {
+    // Error handling, stay here
+    while (1)
+      ;
+  }
+
+  // Set SysTick interrupt priority to highest
+  NVIC_SetPriority(SysTick_IRQn, 0);
 }
 
 void delay_ms(uint32_t ms)
 {
   delay_count = ms;
-  while(delay_count != 0);
+  while (delay_count != 0)
+    ;
 }
 
 void GPIO_Init_LED(void)
 {
-    GPIO_InitTypeDef GPIO_InitStructure;
-    
-    // 1. Enable GPIOF clock
-    RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOF, ENABLE);
-    
-    // 2. Configure PF9 and PF10 pins
-    GPIO_InitStructure.GPIO_Pin = GPIO_Pin_9 | GPIO_Pin_10;
-    GPIO_InitStructure.GPIO_Mode = GPIO_Mode_OUT;
-    GPIO_InitStructure.GPIO_OType = GPIO_OType_OD;
-    GPIO_InitStructure.GPIO_Speed = GPIO_Speed_100MHz;
-    GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_NOPULL;
- 
-    GPIO_Init(GPIOF, &GPIO_InitStructure);
-    
-    // 3. Initialize LED states
-    GPIO_SetBits(GPIOF, GPIO_Pin_9);
+  GPIO_InitTypeDef GPIO_InitStructure;
+
+  // 1. Enable GPIOF clock
+  RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOF, ENABLE);
+
+  // 2. Configure PF9 and PF10 pins
+  GPIO_InitStructure.GPIO_Pin = GPIO_Pin_9 | GPIO_Pin_10;
+  GPIO_InitStructure.GPIO_Mode = GPIO_Mode_OUT;
+  GPIO_InitStructure.GPIO_OType = GPIO_OType_OD;
+  GPIO_InitStructure.GPIO_Speed = GPIO_Speed_100MHz;
+  GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_NOPULL;
+
+  GPIO_Init(GPIOF, &GPIO_InitStructure);
+
+  // 3. Initialize LED states
+  GPIO_SetBits(GPIOF, GPIO_Pin_9);
   GPIO_ResetBits(GPIOF, GPIO_Pin_10);
 }
 
-
 int main(void)
 {
-    Systick_Init();
-    GPIO_Init_LED();
-  
-    while(1)
-    {
+  Systick_Init();
+  GPIO_Init_LED();
+
+  while (1)
+  {
     GPIO_SetBits(GPIOF, GPIO_Pin_9);
     delay_ms(1000);
     GPIO_ResetBits(GPIOF, GPIO_Pin_9);
     delay_ms(1000);
-    }
+  }
 }
